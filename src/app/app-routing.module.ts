388f91fe3876761c7d71b395f5d0840524ed60fa
Pedro Sousa <pedropauloassis@live.com>
2022-10-06 17:28:47 -0300

    agora

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeOneComponent } from './components/pages/home-one/home-one.component';
+
 import { HomeTwoComponent } from './components/pages/home-two/home-two.component';
-import { HomeThreeComponent } from './components/pages/home-three/home-three.component';
-import { HomeFourComponent } from './components/pages/home-four/home-four.component';
-import { HomeFiveComponent } from './components/pages/home-five/home-five.component';
-import { HomeSixComponent } from './components/pages/home-six/home-six.component';
-import { HomeSevenComponent } from './components/pages/home-seven/home-seven.component';
-import { HomeEightComponent } from './components/pages/home-eight/home-eight.component';
+
 import { ContactComponent } from './components/pages/contact/contact.component';
 import { BlogRightSidebarComponent } from './components/pages/blog-right-sidebar/blog-right-sidebar.component';
 import { BlogDetailsComponent } from './components/pages/blog-details/blog-details.component';
